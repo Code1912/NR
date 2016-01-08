37f39e89a0e965a439cbd26f5fa62ca144cd83c2
n1901 <zeng3750@qq.com>
2016-01-08 22:47:09 +0800

    Initial Commit

---
+++ Z-Reader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Z-Reader
+//
+//  Created by Code on 16/1/8.
+//  Copyright © 2016年 Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Z-ReaderTests/Z_ReaderTests.m
@@ -0,0 +1,39 @@
+//
+//  Z_ReaderTests.m
+//  Z-ReaderTests
+//
+//  Created by Code on 16/1/8.
+//  Copyright © 2016年 Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Z_ReaderTests : XCTestCase
+
+@end
+
+@implementation Z_ReaderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
